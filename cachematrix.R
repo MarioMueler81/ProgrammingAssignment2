0c3551c9e5365994ba8cbeb5a8d876a7f67fc5ae
Mario Müller <mariomuller@Marios-Air.fritz.box>
2015-05-19 20:24:33 +0200

    test

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,41 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ##The first function, makeCacheMatrix creates a special "maxtrix", 
+  ##which is really a list containing a function to
+  ##      1. set the value of the vector
+  ##      2. get the value of the vector
+  ##      3. set the value of the invrs
+  ##      4. get the value of the inverse
+  inv = NULL
+  set <- function(y) {
+    
+    ##<<- operator which can be used to assign a value to an 
+    ##object in an environment that is different from the current
+    ##environment. Below are two functions that are used to create 
+    ##a special object that stores a numeric vector and cache's its 
+    ##mean.
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x 
+  setinv <- function(inv) inv <<- inv
+  getinv <- function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+    inv <-  x$getinv()
+    if(!is.null(inv)) {
+      message("getting cached data")
+      return(inv)
+    }
+    mat.data <- x$get()
+    inv <- solve(mat.data, ...)
+    x$setinv(inv)
+    return(inv)
+  }
+
